ef97e7fc238a05663c407bc8c05bc661b2f598c4
sunnyp123 <sunnyparihar35@gmail.com>
2018-03-08 17:37:42 +0530

    adding java file

--- app/src/main/java/com/example/sunnypariharflash/sharelocation/Profile.java
+++ app/src/main/java/com/example/sunnypariharflash/sharelocation/Profile.java
@@ -2,12 +2,31 @@ package com.example.sunnypariharflash.sharelocation;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class Profile extends AppCompatActivity {
-
+ImageView image;
+Button btn;
+EditText edt1,edt2;
+FirebaseAuth auth;
+DatabaseReference reference;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        image = findViewById(R.id.image);
+        btn = findViewById(R.id.update);
+        edt1 = findViewById(R.id.nameedit);
+        edt2 = findViewById(R.id.Emailedit);
+        auth = FirebaseAuth.getInstance();
+        reference = FirebaseDatabase.getInstance().getReference().child("Users");
+
     }
 }
---
+++ app/src/main/java/com/example/sunnypariharflash/sharelocation/ProfileData.java
@@ -0,0 +1,8 @@
+package com.example.sunnypariharflash.sharelocation;
+
+/**
+ * Created by Sunny Parihar on 08-03-2018.
+ */
+
+public class ProfileData {
+}
